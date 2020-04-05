83c198a0cdafb4281352b5803d8d2d8a23b22710
David Winton <dwinton5@gmail.com>
2020-04-05 10:38:04 -0500

    Solution files modified with custom API and marker colors.

---
+++ Leaflet-Level1/static/config.js
@@ -0,0 +1,6 @@
+//API key
+var config = {
+
+    API_KEY:'pk.eyJ1IjoiZHdpbnRvbjUiLCJhIjoiY2s3cnA5eGk4MGdkazNmbXZmNThhczY0bCJ9.6CS1xT5DOYED5XryC9nN-A'
+
+}
---
+++ Leaflet-Level1/static/geomap.js
@@ -0,0 +1,115 @@
+// Geomaps fashioned using code from solutions @ https://vanderbilt.bootcampcontent.com/vanderbilt_coding_bootcamp/VU-NSH-DATA-PT-10-2019-U-C/-/tree/master/17-Mapping-Web/Homework/Solutions/Step-1
+// We create the tile layer that will be the background of our map.
+console.log("working");
+
+var apiKey = 'pk.eyJ1IjoiZHdpbnRvbjUiLCJhIjoiY2s3cnA5eGk4MGdkazNmbXZmNThhczY0bCJ9.6CS1xT5DOYED5XryC9nN-A';
+
+var graymap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.streets",
+  accessToken: apiKey
+});
+
+// We create the map object with options.
+var map = L.map("mapid", {
+  center: [
+    40.7, -94.5
+  ],
+  zoom: 3
+});
+
+// Then we add our 'graymap' tile layer to the map.
+graymap.addTo(map);
+
+// Here we make an AJAX call that retrieves our earthquake geoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson", function(data) {
+
+  // This function returns the style data for each of the earthquakes we plot on
+  // the map. We pass the magnitude of the earthquake into two separate functions
+  // to calculate the color and radius.
+  function styleInfo(feature) {
+    return {
+      opacity: 1,
+      fillOpacity: 1,
+      fillColor: getColor(feature.properties.mag),
+      color: "#000000",
+      radius: getRadius(feature.properties.mag),
+      stroke: true,
+      weight: 0.5
+    };
+  }
+
+  // This function determines the color of the marker based on the magnitude of the earthquake.
+  function getColor(magnitude) {
+    switch (true) {
+    case magnitude > 5:
+        return "#EF003A";
+    case magnitude > 4:
+        return "#EFEB00";
+    case magnitude > 3:
+        return "#00EF2B";
+    case magnitude > 2:
+        return "#EB00EF";
+    case magnitude > 1:
+        return "#0041EF";
+    default:
+        return "#F4F6F7";
+    }
+  }
+
+  // This function determines the radius of the earthquake marker based on its magnitude.
+  // Earthquakes with a magnitude of 0 were being plotted with the wrong radius.
+  function getRadius(magnitude) {
+    if (magnitude === 0) {
+      return 1;
+    }
+
+    return magnitude * 4;
+  }
+
+  // Here we add a GeoJSON layer to the map once the file is loaded.
+  L.geoJson(data, {
+    // We turn each feature into a circleMarker on the map.
+    pointToLayer: function(feature, latlng) {
+      return L.circleMarker(latlng);
+    },
+    // We set the style for each circleMarker using our styleInfo function.
+    style: styleInfo,
+    // We create a popup for each marker to display the magnitude and location of the earthquake after the marker has been created and styled
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+    }
+  }).addTo(map);
+
+  // Here we create a legend control object.
+  var legend = L.control({
+    position: "bottomright"
+  });
+
+  // Then add all the details for the legend
+  legend.onAdd = function() {
+    var div = L.DomUtil.create("div", "info legend");
+
+    var grades = [0, 1, 2, 3, 4, 5];
+    var colors = [
+      "#F4F6F7",
+      "#0041EF",
+      "#EB00EF",
+      "#00EF2B",
+      "#EFEB00",
+      "#EF003A"
+    ];
+
+    // Looping through our intervals to generate a label with a colored square for each interval.
+    for (var i = 0; i < grades.length; i++) {
+      div.innerHTML +=
+        "<i style='background: " + colors[i] + "'></i> " +
+        grades[i] + (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+");
+    }
+    return div;
+  };
+
+  // Finally, we our legend to the map.
+  legend.addTo(map);
+});
